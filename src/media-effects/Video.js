962d1b044918b2caa67f4cf1e05fc73dfb7a0dce
Aaron Abbott <aabmass@gmail.com>
2017-06-22 12:55:12 -0400

    Added build steps for just the library
    This whole repo is structured as a create-react-app, but the actually
    lib which will contain the react components for media effects will be
    inside of src/media-effects/. When the build happens, only js files in
    that directory will be compiled and put into lib/ output directory.

---
+++ src/media-effects/Video.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Video extends Component {
+  render() {
+    return <h3>Video Here... Hello World!</h3>
+  }
+}
+
+/* export as named and default */
+export {
+  Video
+};
+
+export default Video;
