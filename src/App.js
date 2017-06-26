e608254778b4b82522f8f89849d56da6ae7bfe35
Aaron Abbott <aabmass@gmail.com>
2017-06-25 22:55:42 -0400

    Split example into separate comp, wrote README

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import Video from './react-media-effects';
-
-function alphaTopCornerFilter(frameData) {
-  // for testing, just set top right corner partially transparent with alpha
-  // channel
-  for (var i = frameData.width - 1; i > frameData.width - 200; --i) {
-    for (var j = 0; j < 100; ++j) {
-      frameData.set(i, j, {
-        a: 200
-      });
-    }
-  }
-}
-
-function horizontalDerivative(frameData) {
-  // an edge detection effect
-  for (var i = 0; i < frameData.width - 1; ++i) {
-    for (var j = 0; j < frameData.height; ++j) {
-      const right = frameData.get(i + 1, j);
-      const left = frameData.get(i, j);
-      let diff = {
-        r: right.r - left.r,
-        g: right.g - left.g,
-        b: right.b - left.b,
-      }
-      const scale = 10;
-      diff.r *= scale;
-      diff.g *= scale;
-      diff.b *= scale;
-
-      frameData.set(i, j, diff);
-    }
-  }
-}
+import Example from './Example';
 
 class App extends Component {
   state = {
@@ -58,21 +24,7 @@ class App extends Component {
         </div>
 
         <div id="main">
-          <h2>Here are some effects:</h2>       
-
-          {/* open source movie: https://archive.org/details/Glass_201703 */}
-          <Video
-            src="Glass.mp4"
-            className="centered"
-            width="600"
-            controls
-            filter={horizontalDerivative}
-            play={this.state.play}
-          />
-
-          <div className="controls">
-            <button onClick={this.onPlayPause}>Play/Pause</button>
-          </div>
+          <Example />
         </div>
       </div>
     );
---
+++ src/Example.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import Video from './react-media-effects';
+
+const greenFilter = (frameData) => {
+  // increase the green channel of every pixel in the frame for a green tint
+  for (var i = 0; i < frameData.width - 1; ++i) {
+    for (var j = 0; j < frameData.height; ++j) {
+      const pix = frameData.get(i, j);
+      pix.g += 50;
+      frameData.set(i, j, pix);
+    }
+  }
+};
+
+// eslint-disable-next-line
+const rowDiffFilter = (frameData) => {
+  // an edge detection effect by taking the difference across a row
+  for (var i = 0; i < frameData.width - 1; ++i) {
+    for (var j = 0; j < frameData.height; ++j) {
+      const right = frameData.get(i + 1, j);
+      const left = frameData.get(i, j);
+      let diff = {
+        r: right.r - left.r,
+        g: right.g - left.g,
+        b: right.b - left.b,
+      }
+      const scale = 10;
+      diff.r *= scale;
+      diff.g *= scale;
+      diff.b *= scale;
+
+      frameData.set(i, j, diff);
+    }
+  }
+};
+
+class Example extends Component {
+  state = {
+    play: true
+  }
+
+  onPlayPause = e => {
+    // toggle
+    this.setState({
+      play: !this.state.play
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Here are some effects:</h2>
+
+        {/* open source movie: https://archive.org/details/Glass_201703 */}
+        <Video
+          src="Glass.mp4"
+          className="centered"
+          width="600"
+          filter={greenFilter}
+          play={this.state.play}
+        />
+
+        <div className="controls">
+          <button onClick={this.onPlayPause}>Play/Pause</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Example;
